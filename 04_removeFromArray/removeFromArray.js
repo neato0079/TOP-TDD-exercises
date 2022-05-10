465b1c83244f49e280f46829b2ce87b283690b7a
Matt Bott <neato0079@gmail.com>
2022-05-10 10:33:51 -0700

    removed excess comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,3 @@
-// const removeFromArray = function(array, removeNumber) {
-//     let result = array;
-//     for(let i= 1;i < arguments.length; i++){
-//         console.log(arguments[i])
-//         result = array.filter(number => number !== arguments[i])
-//         console.log(result)
-//     }
-//     return result;
-// };
-
-// console.log(removeFromArray([1, 2, 3, 4], 3, 2))
-
 const removeFromArray = function(array, ...removeNumber) {
     // let result = [];
     
@@ -21,17 +9,6 @@ const removeFromArray = function(array, ...removeNumber) {
     // return result
     return result = array.filter(num => !(removeNumber.includes(num)));
 };
-
-//console.log(removeFromArray([1, 2, 3, 4], 3, 2))
-
-
-
-
-
-
-
-
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
