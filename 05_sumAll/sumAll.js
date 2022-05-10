dbefa6cd18f20f09c18bb538f482559a2ce24c7e
Matt Bott <neato0079@gmail.com>
2022-05-10 10:52:14 -0700

    Solved problem 05 for only half of the requirements of the test unit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let smallNum = num1;
+    let largeNum = num2;
+    if(num1 > num2){
+        smallNum = num2;
+        largeNum = num1;
+    }
+    
+    //use a traditional for loop and i to num1 and the length part of the loop to num2
+    //add i to sum
+    for(i = smallNum; i <= largeNum; i++){
+        sum += i;
+    }
 
+    return sum;
 };
+
+console.log(sumAll(4,1))
 
 // Do not edit below this line
 module.exports = sumAll;
