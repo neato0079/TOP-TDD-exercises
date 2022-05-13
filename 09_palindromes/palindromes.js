16032d446848b617d046c2a6f283dc22f21f3f78
Matt Bott <neato0079@gmail.com>
2022-05-13 16:15:49 -0700

    Solved but messy?

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toUpperCase()
+    let wordArray = word.split('')
+    //filter out punc
+    const noPunct = wordArray.filter(char => !(char.charCodeAt(0) < 48 || char.charCodeAt(0) > 90)).join('')
+    
+    const reverse = noPunct.split('').reverse().join('');
+    if(noPunct === reverse){
+        return true;
+    }else{
+        return false;
+    }
 };
+
+console.log(palindromes('Racecar!'))
 
 // Do not edit below this line
 module.exports = palindromes;
