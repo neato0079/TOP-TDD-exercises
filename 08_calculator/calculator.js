38769ea47b4571c8d27c9bea3ac5f09c7edc1e20
Matt Bott <neato0079@gmail.com>
2022-05-13 15:08:23 -0700

    Solved. May be refactored further

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, val) => sum + val, 0); //initializer must be include to account for the case of an empty array
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  // let product = 1;
+  // array.forEach(num =>{
+  //   product *= num;
+  // })
+  // return product
+  return array.reduce((prod, val) => prod * val, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let power = 1
+	for(i = 0; i < exp; i++){
+    power *= base;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0){
+    return 1;
+  }
+  let factorial = num;
+	for(i = num - 1; i >= 1; i--){
+    factorial = factorial * i
+  }
+  return factorial;
 };
+
+// console.log(factorial(7))
 
 // Do not edit below this line
 module.exports = {
