54497ade6165d88f7251775fa64a0d93a244217a
neato0079 <neato0079@gmail.com>
2022-05-13 06:53:22 -0700

    Add initial solution. Need to fix decimal result

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const ftoc = function() {
+// [°F] = [°C] × 9⁄5 + 32 
 
+const ftoc = function(temp) {
+  const celsius = (temp - 32) * 5/9;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const fahrenheit = temp * 9/5 + 32;
+  return fahrenheit;
 };
 
+console.log(ftoc(100))
 // Do not edit below this line
 module.exports = {
   ftoc,
