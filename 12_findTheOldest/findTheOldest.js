1bf2b6bf27467010491e01ce4095d1052394236a
neato0079 <neato0079@gmail.com>
2022-05-14 06:49:06 -0700

    Add small progress of skeleton

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeople) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    let age = 0
+    age = arrayOfPeople[0].yearOfBirth - arrayOfPeople[0].yearOfDeath;
 };
 
 // Do not edit below this line
