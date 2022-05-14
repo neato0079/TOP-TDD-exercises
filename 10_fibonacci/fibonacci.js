379d4115a22ebe65f310e39e6f3bbd04129c6ccc
neato0079 <neato0079@gmail.com>
2022-05-14 02:34:00 -0700

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0){
+        return 'OOPS'
+    }
+    let fib = []; // Initialize array!
 
+    fib[0] = 0;
+    fib[1] = 1;
+    for (i = 2; i <= num; i++) {
+        // Next fibonacci number = previous + one before previous
+        // Translated to JavaScript:
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+        return fib[num];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
