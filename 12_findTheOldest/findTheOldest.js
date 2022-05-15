72d1be271668e62af1ee537e3496c2cd5c9e8cdf
neato0079 <neato0079@gmail.com>
2022-05-15 01:04:20 -0700

    Solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,30 @@
 const findTheOldest = function(arrayOfPeople) {
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
+    let oldest = 0
     let age = 0
-    age = arrayOfPeople[0].yearOfBirth - arrayOfPeople[0].yearOfDeath;
+    //find the oldest age
+    for(let person of arrayOfPeople){
+        if(!(person.yearOfDeath)){
+            age = currentYear - person.yearOfBirth;
+        }else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if(oldest < age){
+            oldest = age;
+        }
+    }
+    //find which person has the oldest age
+    for(let person of arrayOfPeople){
+        if(!(person.yearOfDeath)){
+            age = currentYear - person.yearOfBirth;
+        }else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if(oldest === age){
+            return person
+        }
+    }
 };
 
 // Do not edit below this line
