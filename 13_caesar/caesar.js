54bf9a4d52eed2329eb347bf42291f5ea52b4057
Matt Bott <neato0079@gmail.com>
2022-05-18 16:01:35 -0700

    Pass 3 of 7 tests

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,16 +1,94 @@
 const caesar = function(string, rightShiftCount) {
-    let ascii = [];
+    //convert string to lower case
+    let lowerString = string.toLowerCase()
+
+    //convert string to ascii
+    let asciiArray = [];
     for(i = 0; i < string.length; i++){
-        ascii.push(string[i].charCodeAt(0))
+        //only convert ascii values over 96
+        let currentCharInASCII = lowerString[i].charCodeAt(0);
+        if(currentCharInASCII > 96){
+            asciiArray.push(currentCharInASCII)
+        }else{
+            asciiArray.push(lowerString[i])
+        }
     }
-    return ascii
+
+    //apply shift to ascii array
+    asciiArray = asciiArray.map(num => {
+        if(typeof num === 'number'){
+            return num + rightShiftCount      
+        }else{
+            return num
+        }
+    })
+
+    //apply wrap
+    asciiArray = asciiArray.map(num => {
+        if(num > 122){
+            return num - 26      
+        }else{
+            return num
+        }
+    })
+
+
+    //convert ascii array to char array
+    let charArray = asciiArray.map(char => {
+        if(typeof char === 'number'){
+            return String.fromCharCode(char)      
+        }else{
+            return char
+        }
+    })
+
+    //reapply capitalization
+    // charArray = charArray.map(char =>{
+    //     for(i = 0; i < string.length; i++){
+    //         if(string[i].toUpperCase() === string[i]){
+    //             return char.toUpperCase()
+    //         }else{
+    //             return char
+    //         }
+    //     }
+    // })
+
+    // charArray = charArray.map(char =>{
+    //     console.log(charArray.indexOf(char))
+    //     if(string[charArray.indexOf(char)].toUpperCase() === string[charArray.indexOf(char)]){
+    //         return char.toUpperCase()
+    //     }else{
+    //         return char
+    //     }
+        
+    // })
+
+    
+
+    //reapply capitalization
+    let charArrayWithCaps = []
+    for(i = 0; i < string.length; i++){
+        if(string[i].toUpperCase() === string[i]){
+            charArrayWithCaps.push(charArray[i].toUpperCase());
+        }else{
+            charArrayWithCaps.push(charArray[i]);
+        }
+    }
+
+
+    //convert char array to string
+    let caesarString = charArrayWithCaps.join('')
+    
+
+    return caesarString
     //ASCIIaphabet.map(letter => {
 //         letter + rightShift
 //     })
 // };
 }
-const hello = 'hello'
-console.log(caesar('hello', 0))
+const message = 'Hello, World!'
+const word = 'z'
+console.log(caesar(message, 5))
 // console.log(hello[2].charCodeAt())
 // Do not edit below this line
 module.exports = caesar;
---
+++ 13_caesar/test.js
@@ -0,0 +1,11 @@
+const word = 'heLlo!'
+const anotherword = word.toLowerCase()
+
+const testArray = ['a','b','c']
+
+let i = 0;
+const nums = testArray.map(char =>{
+    return testArray.indexOf(char)
+
+})
+console.log(nums)
