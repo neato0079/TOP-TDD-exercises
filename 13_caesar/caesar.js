3df2e5f7662f256c4e44532e4565abce0ddbff2c
neato0079 <neato0079@gmail.com>
2022-05-18 04:49:00 -0700

    Main function converts string to ascii

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,25 @@
-const caesar = function() {
-
-};
-
+const caesar = function(string, rightShiftCount) {
+    let ascii = [];
+    for(i = 0; i < string.length; i++){
+        ascii.push(string[i].charCodeAt(0))
+    }
+    return ascii
+    //ASCIIaphabet.map(letter => {
+//         letter + rightShift
+//     })
+// };
+}
+const hello = 'hello'
+console.log(caesar('hello', 0))
+// console.log(hello[2].charCodeAt())
 // Do not edit below this line
 module.exports = caesar;
+
+// convert char to ascii
+// add num to ascii
+// reconvert ascii to char
+
+// how do you wrap the alphabet though?
+// if num after addition is larger than the last ascii val (for z), then subtract total ascii alphabet value from the first sum. now you have the ascii value post wrap
+
+// ezpz??????????????
